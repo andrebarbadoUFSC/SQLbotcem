28ccde701affab50b12e8dbe1477bae5dcdf8c9b
André Luís Corrêa Barbado <andre.barbado@grad.ufsc.br>
2022-05-04 11:01:37 -0300

    add members to table

---
+++ Addmembers.sql
@@ -0,0 +1,9 @@
+INSERT INTO members VALUES ('17201609','Andre Luis Correa Barbado','Lider');
+INSERT INTO members VALUES ('17201634','Malu Quinha','Vice Lider');
+INSERT INTO members VALUES ('17201654','Joao Trasvolta','Aposentado ou pensionista');
+INSERT INTO members VALUES ('17201689','Caroulo Jucas','Seguindo a vida');
+INSERT INTO members VALUES ('17201622','Nina Não','Dando golpe de estado');
+
+
+
+SELECT * FROM members /* a estrela serve pra seleecionar todos */
