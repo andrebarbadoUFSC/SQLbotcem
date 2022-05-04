3ef60f3f15906f37dfac09e9f846e5355fc1c238
André Luís Corrêa Barbado <andre.barbado@grad.ufsc.br>
2022-05-04 11:19:19 -0300

    Adding projects

---
+++ AddProjects.sql
@@ -0,0 +1,7 @@
+INSERT INTO projects VALUES (1,'Adm'); 
+INSERT INTO projects VALUES (2,'Seguidor'); 
+INSERT INTO projects VALUES (3,'Sumo'); 
+INSERT INTO projects VALUES (4,'VSS'); 
+
+
+SELECT * FROM projects /* a estrela serve pra seleecionar todos */
